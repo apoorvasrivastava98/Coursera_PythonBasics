ab117bb43f747fa357e5b241d5b62fd52a062d36
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-06-05 20:46:35 +0530

    Create turtleprogram.py

---
+++ turtleprogram.py
@@ -0,0 +1,6 @@
+import turtle #allows us to use turtle library
+wn=turtle.screen() #creates a graphics window
+alex=turtle.Turle() #creates a turtle named alex
+alex.forward(150) #tell alex to move forwardby 150 units
+alex.left(90) #turn by 90 degrees
+alex.forward(75) #complete second side of a rectangle
