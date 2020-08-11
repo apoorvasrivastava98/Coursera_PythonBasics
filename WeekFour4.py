07341b68c77ddcd4aa0076e20c4d175ebc003603
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-08-12 01:11:01 +0530

    Create WeekFour4.py

---
+++ WeekFour4.py
@@ -0,0 +1,12 @@
+ Q-What will be the value of a after the following code has executed?
+
+a = ["holiday", "celebrate!"]
+quiet = a
+quiet.append("company")
+The value of a will be
+ans-['holiday', 'celebrate!', 'company']
+
+Q-For each character in the string saved in ael, append that character to a list that should be saved in a variable app.
+   
+   
+Q-For each string in wrds, add ‘ed’ to the end of the word (to make the word past tense). Save these past tense words to a list called past_wrds.
