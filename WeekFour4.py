446ecf05c8e8d75039764916ed519e26942eb81c
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-08-12 01:12:16 +0530

    Update WeekFour4.py

--- WeekFour4.py
+++ WeekFour4.py
@@ -7,6 +7,10 @@ The value of a will be
 ans-['holiday', 'celebrate!', 'company']
 
 Q-For each character in the string saved in ael, append that character to a list that should be saved in a variable app.
-   
+   ael = "python!"
+   app = []
+   for character in ael:
+      app.append(character)
    
 Q-For each string in wrds, add ‘ed’ to the end of the word (to make the word past tense). Save these past tense words to a list called past_wrds.
+    
