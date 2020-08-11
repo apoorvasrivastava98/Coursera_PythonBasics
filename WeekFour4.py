852c3d0ccb71941b6eece99f3c1770c89490c736
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-08-12 01:14:03 +0530

    Update WeekFour4.py

--- WeekFour4.py
+++ WeekFour4.py
@@ -13,4 +13,8 @@ Q-For each character in the string saved in ael, append that character to a list
       app.append(character)
    
 Q-For each string in wrds, add ‘ed’ to the end of the word (to make the word past tense). Save these past tense words to a list called past_wrds.
-    
+    wrds = ["end", 'work', "play", "start", "walk", "look", "open", "rain", "learn", "clean"]
+    past_wrds = []
+    for word in wrds:
+        word += 'ed'
+        past_wrds.append(word)
